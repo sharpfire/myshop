ac54c20d00c4d99f23537beb52ea9669812dc80a
scott <hellangelzy@hotmail.com>
2017-07-05 17:34:47 +0000

    (chapter 7) register models to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from shop.models import Product,Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    #我们使用 prepopulated_fields 属性来指定那些要使用其
+    # 他字段来自动赋值的字段。
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product,ProductAdmin)
